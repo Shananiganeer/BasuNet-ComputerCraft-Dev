01a359f77f203b20b6a796be1f029ec1541f6682
Shananiganeer <ravi.n.basu@gmail.com>
2019-03-14 02:58:42 -0400

    typo again

--- ReactOS/startup.lua
+++ ReactOS/startup.lua
@@ -5,8 +5,8 @@ paths[#paths+1] = "ReactOS/startup.lua"
 paths[#paths+1] = "apis/wp.lua"
 paths[#paths+1] = "apis/gh.lua"
 
-if not fs.exists("apis/gh.lua") then
-  local h, f = http.get(repo..paths[4]), paths[4]
+if not fs.exists(paths[4]) then
+  local h, f = http.get(repo..paths[4]), fs.open(paths[4])
   f.write(h.readAll())
   h.close()
   f.close()
